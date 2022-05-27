2e3fd40ba4560dcbf12386b3bbf92acc6ab7871d
alekseykka <59233823+alekseykka@users.noreply.github.com>
2022-05-27 15:42:05 +0300

    code editing

--- list_dict/car accounting/main.py
+++ list_dict/car accounting/main.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 def add(contact:dict):
     name = input('Введите имя').title()
     if contact.get(name):
@@ -25,22 +27,39 @@ def change(contact:dict):
         contact[name] = input('Введите машину')
     return contact
 
+def save_file(cars:dict):
+    with open('contact.txt', 'w', encoding='utf-8') as file:
+        for k,v in cars.items():
+            file.write(f'{k}:{v}\n')
+        print('Машины записаны')
+
+def read_file():
+    f = Path('contact.txt')
+    cars = {}
+    if f.exists():
+        with open('contact.txt', 'r', encoding='utf-8') as file:
+            for i in file.readlines():
+                name,car = i[:-1].split(':')
+                cars.update({name: car})
+    else:
+        file = open('contact.txt', 'w', encoding='utf-8')
+        file.close()
+    return cars
+
 def main():
-    with open('contact.txt', 'r+', encoding='utf-8') as file:
-        contact = file.read()
-
-    contact = {'Alex': 'BMW', 'Lena': 'Audi', 'Vova': 'Volvo'}
+    cars = read_file()
     job = {'1': add, '2': delete, '3': print_inf, '4': change}
-
     while True:
         jobs = input('1 - Добавить\n2 - Удалить\n3 - Просмотр\n4 - Изменить машину'
-                     '\n5 - Выход\nВведите команду')
-        if jobs == '5':
+                     '\n5 - Запись в файл\n6 - Выход\nВведите команду ')
+        if jobs == '6':
             break
-        contact = job[jobs](contact)
+        elif jobs == '5':
+            save_file(cars)
+        else:
+            cars = job[jobs](cars)
 
-    with open('contact.txt', 'r+', encoding='utf-8') as file:
-        file.write(str(contact))
+
 
 if __name__ == '__main__':
     main()
